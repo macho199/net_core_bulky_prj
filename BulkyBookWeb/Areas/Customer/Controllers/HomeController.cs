66f211a1f710b5c3675d2f44e1d1d8365a0bc82f
kjs <macho199@gmail.com>
2023-03-18 12:41:02 +0900

    Repository 섹션 완료

---
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BulkyBook.Models;
+
+namespace BulkyBookWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration Configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        Configuration = configuration;
+        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+}
+
